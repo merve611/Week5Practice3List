645c7a1df684c6fa653f3ec49e75e97d2fdb00cd
merve cakiroglu <mrveeckrrr@hotmail.com>
2024-08-14 15:55:47 +0300

    Add project files.

---
+++ Week5Practice3List/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> coffee = new List<string>();           //Kahve listesi oluşturuldu
+
+
+        Console.WriteLine("Bir kahve içsek bize iyi gelecek!");
+        Console.WriteLine("5 adet kahve ismi giriniz : ");
+        int sayac = 1;
+        string coffeeName = "";
+
+        while(sayac<=5)      //kullanıcıdan 5 adet kahve ismi istendi Döngüye 5 kez girecek
+        {
+            Console.Write("Kahve " + sayac + ":" );
+            coffeeName = Console.ReadLine();
+
+            coffee.Add(coffeeName);         //listeye kahve isimleri eklendi
+
+            sayac++;
+
+
+        }
+
+        Console.WriteLine("Girilen Kahve İsimleri: ");          
+
+        foreach (string cof in coffee)          //Kahve listesi foreach döngüsüyle ekrana yazıldı
+        {
+            Console.WriteLine(cof);
+        }
+    }
+}
